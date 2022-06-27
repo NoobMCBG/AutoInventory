c3b98f720e8e942d32961733c7b1738de122a0eb
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2022-06-27 12:47:17 +0700

    Update AutoInventory.php

--- src/NoobMCBG/AutoInventory/AutoInventory.php
+++ src/NoobMCBG/AutoInventory/AutoInventory.php
@@ -21,6 +21,7 @@ class AutoInventory extends PluginBase {
     public function onEnable() : void {
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
         $this->saveDefaultConfig();
+        $this->checkUpdate();
         self::$instance = $this;
     }
     
@@ -31,9 +32,54 @@ class AutoInventory extends PluginBase {
         if($player->getInventory()->canAddItem($item)){
             $player->getInventory()->addItem($item);
         }else{
-            if($this->getConfig()->get("full-inv-drop") == true){
+            if($this->getConfig()->getAll()["full-inv"]["drop"] == true){
                 $player->getWorld()->dropItem($player->getPosition()->asVector3(), $item);
+            }else{
+                if($this->getConfig()->getAll()["full-inv"]["title"]["mode"] == true){
+                    $player->sendTitle($this->getConfig()->getAll()["full-inv"]["title"]["format"]);
+                    if(is_array($this->getConfig()->getAll()["full-inv"]["title"]["sounds"])){  
+                        $this->playArraySound($player, $this->getConfig()->getAll()["full-inv"]["title"]["sounds"]);
+                    }else{
+                        $this->playSound($player, $this->getConfig()->getAll()["full-inv"]["title"]["sounds"]);
+                    }
+                }
             }
         }
     }
+    
+    /**
+     * @param Player $player
+     * @param string $soundName
+     * @param float $volume = 0
+     * @param float $pitch = 0
+     */
+	public function playSound(Player $player, string $soundName, float $volume = 0, float $pitch = 0) : void {
+		$packet = new PlaySoundPacket();
+		$packet->soundName = $soundName;
+		$packet->x = $player->getPosition()->getX();
+		$packet->y = $player->getPosition()->getY();
+		$packet->z = $player->getPosition()->getZ();
+		$packet->volume = $volume;
+		$packet->pitch = $pitch;
+		$player->getNetworkSession()->sendDataPacket($packet);
+	}
+    
+    /**
+     * @param Player $player
+     * @param array $soundName
+     * @param float $volume = 0
+     * @param float $pitch = 0
+     */
+	public function playArraySound(Player $player, array $sound, float $volume = 0, float $pitch = 0) : void {
+        foreach($sound as $soundName){
+            $packet = new PlaySoundPacket();
+            $packet->soundName = $soundName;
+            $packet->x = $player->getPosition()->getX();
+            $packet->y = $player->getPosition()->getY();
+            $packet->z = $player->getPosition()->getZ();
+            $packet->volume = $volume;
+            $packet->pitch = $pitch;
+            $player->getNetworkSession()->sendDataPacket($packet);
+        }
+	}
 }

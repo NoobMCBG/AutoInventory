e9ebb926b9a3a77003fc0425872f87bffdfa8d12
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2022-06-27 12:06:32 +0700

    Create AutoInventory.php

---
+++ src/NoobMCBG/AutoInventory/AutoInventory.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NoobMCBG\AutoInventory;
+
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use NoobMCBG\AutoInventory\task\CheckUpdateTask;
+
+class AutoInventory extends PluginBase {
+    
+    public static $instance;
+}

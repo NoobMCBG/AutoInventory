02f33a1dd9dd1a2674dadbe74b0c1716c8b2f5c3
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2022-06-27 12:20:19 +0700

    Update EventListener.php

--- src/NoobMCBG/AutoInventory/EventListener.php
+++ src/NoobMCBG/AutoInventory/EventListener.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace NoobMCBG\AutoInventory;
 
+use pocketmine\player\Player;
 use pocketmine\event\Listener;
 use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDeathEvent;
+use pocketmine\event\entity\EntityExplodeEvent;
 
 class EventListener implements Listener {
     
@@ -14,4 +18,30 @@ class EventListener implements Listener {
     public function __construct(AutoInventory $plugin){
         $this->plugin = $plugin;
     }
+    
+    /**
+	 * @param BlockBreakEvent $event
+	 */
+	public function onBreak(BlockBreakEvent $event) : void {
+		foreach($event->getDrops() as $drop){
+			$this->plugin->autoInventory($event->getPlayer(), $drop);
+		}
+		$event->setDrops([]);
+	}
+    
+    /**
+	 * @param EntityDeathEvent $event
+	 */
+    public function onDeath(EntityDeathEvent $event) : void {
+		$cause = $event->getEntity()->getLastDamageCause();
+		if($cause instanceof EntityDamageByEntityEvent){
+			$damager = $cause->getDamager();
+			if($damager instanceof Player){
+				foreach($event->getDrops() as $drop) {
+					$this->plugin->autoInventory($damager, $drop);
+				}
+				$event->setDrops([]);
+			}
+		}
+	}
 }
